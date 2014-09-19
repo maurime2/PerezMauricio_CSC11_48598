bd19a46ef78033dfc51e22aa536e891ab24a6ee2
maurime2 <maurime2@gmail.com>
2014-09-19 16:01:18 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

---
+++ Book/Prog6a.s
@@ -0,0 +1,11 @@
+/*Preform R0=r1+r2*/
+
+	.global _start
+-start:
+	MOV R1, #50		/*Get 50 into R1*/
+	MOV R2, #60		/*Get 60 into R2*/
+	ADD RO, R1, R2	/*Add the two into r2*/
+	
+	MOV R7, #1	/*exit through system call*/
+	SWI 0
+	
