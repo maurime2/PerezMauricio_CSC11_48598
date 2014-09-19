1348dfba187e38755b009f5ed7274ce3168ddd1c
maurime2 <maurime2@gmail.com>
2014-09-19 16:06:19 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- Book/Prog6a.s
+++ Book/Prog6a.s
@@ -4,7 +4,7 @@
 -start:
 	MOV R1, #50		/*Get 50 into R1*/
 	MOV R2, #60		/*Get 60 into R2*/
-	ADD RO, R1, R2	/*Add the two into r2*/
+	ADDs R0, R1, R2	/*Add the two into r2*/
 	
 	MOV R7, #1	/*exit through system call*/
 	SWI 0
