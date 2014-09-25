a785aff6e29efbdb33467a5b60c0d4cf6a94f618
maurime2 <maurime2@gmail.com>
2014-09-24 19:03:36 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- Book/prog7a.s
+++ Book/prog7a.s
@@ -5,7 +5,7 @@ _start:
 
 	MOV R7, #4		@ Syscall number
 	MOV R0, #1		@ Stdout is monitor
-	MOV R2, #17		@ string is 19 chars long
+	MOV R2, #11		@ string is 19 chars long
 	LDR R1,=string	@ string located at string
 	SWI 0
 	
