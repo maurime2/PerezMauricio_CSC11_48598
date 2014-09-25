0972d8d07b1002076f88327780f1cce55ca5e44b
maurime2 <maurime2@gmail.com>
2014-09-24 22:11:04 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- Book/prog8b.s
+++ Book/prog8b.s
@@ -20,3 +20,25 @@ _bits:
 _print0:
 	MOV R8, R6				@ MOV preserve, no
 	MOV R0, #48				@ ACII '0'
+	STR R0, [R1]			@ Store 0 in string
+	BL _write
+	MOV R6, R8				@ MOV no, preserve
+
+_noprint1:
+	MOVS R9, R9, LSR #1		@ shuffle mask bits
+	BNE _bits
+
+_exit:
+	MOV R7, #1
+	SWI 0
+
+_write:
+	MOV R0, #1
+	MOV R2, #1
+	MOV R7, #4
+	SWI 0
+	MOV PC, LR
+
+.data
+string:
+	.ascii " "
