de77daa0aebf0ac31c146c14d4da161d3951e9a1
maurime2 <maurime2@gmail.com>
2014-09-24 18:59:44 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

---
+++ Book/prog7a.s
@@ -0,0 +1,19 @@
+/**/
+
+	.global _start
+_start:
+
+	MOV R7, #4		@ Syscall number
+	MOV R0, #1		@ Stdout is monitor
+	MOV R2, #19		@ string is 19 chars long
+	LDR R1,=string	@ string located at string
+	SWI 0
+	
+_exit:
+
+	MOV R7, #1
+	SWI 0
+	
+.data
+string:
+.ascii "Hello World String\n"
