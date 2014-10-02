4db6bb6185f61830c6ac4ccdcba0d39b47252288
maurime2 <maurime2@gmail.com>
2014-10-01 22:59:16 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- Book/Divide.s
+++ Book/Divide.s
@@ -10,12 +10,14 @@ _sub:						@ Subtraction Happens HERE.
 	SUBS R0, R4, R5
 	BNE _exit				@ If Negative, go to exit.
 	
-_add						@ Increment R6 HERE
+_add;						@ Increment R6 HERE
 	ADD R6, R6, #1			@INCREMENT R7
 	TST R6, R6
 	BEQ _sub
 	
 _exit:
+	ADD R0, R4, R5
+	@ MOV R0, R6
 	MOV R7, #1
 	SWI 0
 	
