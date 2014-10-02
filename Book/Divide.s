fb85b0da2b429addcc4249cbe226cc2f1ced1dd0
maurime2 <maurime2@gmail.com>
2014-10-01 23:34:22 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- Book/Divide.s
+++ Book/Divide.s
@@ -5,19 +5,17 @@ _start:
 	MOV R4, #110			@ Number to Divide/ Will hold Remander at the end.
 	MOV R5, #5				@ Number to Divide By.
 	MOV R3, #0				@ Number to hold Value.
-	SUBS R1, R4, R5
-	ADDS R3, R3, #1			@INCREMENT R7
 	
 _sub:						@ Subtraction Happens HERE.
 	SUBS R1, R1, R5
 	BNE _exit				@ If Negative, go to exit.
-	BAL _sub
+							@ BAL _sub @ TEST - SHOULD LOOP TILL NEG VALUE ACHIVED
 _add:						@ Increment R6 HERE
 	ADD R3, R3, #1			@INCREMENT R7
 	BAL _sub
 	
 _exit:
-	ADD R0, R1, #0
+	@ ADD R0, R1, #0
 	@ ADD R0, R3, #0
 	MOV R7, #1
 	SWI 0
