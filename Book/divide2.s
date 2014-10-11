057dfeafe12ed564b437576b14cb66bc59b2b32d
maurime2 <maurime2@gmail.com>
2014-10-10 20:22:42 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- Book/divide2.s
+++ Book/divide2.s
@@ -20,7 +20,7 @@ _add:						@ Increment R3 HERE
 	BAL _sub				@ Always Branch to _sub
 
 _exit:
-	ADD R0, R4, R5			@ Corrects the extra subtraction, a%b = R0
+	ADDs R0, R4, R5			@ Corrects the extra subtraction, a%b = R0
 	@ ADD R0, R1, #0		@ Test: Shows R4/R5 in R1
 	MOV R7, #1				@ Prepare Software Interrupt 1: EXIT
 	SWI 0					@ Software Interrupt: Exit
