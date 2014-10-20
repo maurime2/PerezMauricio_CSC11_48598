5e2ca24549f82e9a8debb34a58bd97177d01d65b
maurime2 <maurime2@gmail.com>
2014-10-20 12:27:53 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

---
+++ midterm/menu.s
@@ -0,0 +1,52 @@
+/*
+Mauricio S. Perez
+Midterm: CSC11-48598
+Menu
+*/
+
+.data
+/* Problem Chosen */
+.balign 4
+prob: .asciz "Actual Answer a/b = %d\n"
+
+/* Select Message */
+.balign 4
+select: .asciz "Select a Problem: "
+
+/* Format pattern for scanf */
+.balign 4
+scan_pattern : .asciz "%d" 
+
+/* Choice */
+.balign 4
+choice: .word 0
+ 
+.balign 4
+return: .word 0
+
+ 
+.text
+ 
+.global main
+main:
+	/*Print*/
+	ldr r1, address_of_return        /* r1 ? &address_of_return */
+    str lr, [r1]                     /* *r1 ? lr */
+
+    ldr r0, address_of_select     /* Load Address of Select */
+    bl printf                        /* call to printf */
+
+	/*Exit*/
+	MOV R7, #1				@ Prepare Software Interrupt 1: EXIT
+	SWI 0					@ Software Interrupt: Exit
+
+/*Messages*/
+address_of_prob : .word prob
+address_of_select : .word select
+address_of_scan_pattern : .word scan_pattern
+address_of_choice : .word choice
+address_of_return : .word return
+
+/* External */
+.global printf
+.global scanf
