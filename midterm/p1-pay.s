2d5c4c697a9ee8378524e16eb11fd9f69e1cffc4
maurime2 <maurime2@gmail.com>
2014-10-22 02:38:14 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- midterm/p1-pay.s
+++ midterm/p1-pay.s
@@ -39,6 +39,18 @@ hours: .word 0
 .balign 4
 total: .word 0
 
+/* Pay1 */
+.balign 4
+pay1: .word 8
+
+/* Pay2 */
+.balign 4
+pay2: .word 16
+
+/* Pay3 */
+.balign 4
+pay3: .word 24
+
 /*Return*/
 .balign 4
 return: .word 0
@@ -85,6 +97,9 @@ address_of_select3 : .word select3				/*"Problem 3: "*/
 address_of_select4 : .word select4				/*"End Prompt"*/
 address_of_hours : .word hours					/*Address of Hours*/
 address_of_total : .word total					/*Address of Total Pay*/
+address_of_pay1 : .word total					/*Address of Pay1*/
+address_of_pay2 : .word total					/*Address of Pay2*/
+address_of_pay3 : .word total					/*Address of Pay3*/
 address_of_hwork : .word hwork				/*Address of Hours Worked*/
 address_of_return : .word return				/*Address of Return*/
 
