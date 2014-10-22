e53272da94727956d3de0d54081fa6888d35b582
maurime2 <maurime2@gmail.com>
2014-10-22 11:35:47 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- midterm/p1-pay.s
+++ midterm/p1-pay.s
@@ -91,8 +91,8 @@ _start1:
     bl printf                       /* call to printf 
 	
 _h1:
-	mov r2, 0						/*Holds Total pay for class amount*/
-	mov r4, 0						/*Holds count for total hours worked(of each class)*/
+	mov r2, #0						/*Holds Total pay for class amount*/
+	mov r4, #0						/*Holds count for total hours worked(of each class)*/
 	mov r5, address_of_hours		/*Holds address of total hours (all hours)*/
 	ldr r5, r5						/*Holds total hours (all hours)*/
 	mov r6, address_of_hwork		/*Holds address of hours worked for class (For save and print)*/
