ae45d95b5c1bc195cdb93ec234e33f9b84fd34a4
maurime2 <maurime2@gmail.com>
2014-10-23 05:33:06 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- midterm/p3-fib.s
+++ midterm/p3-fib.s
@@ -102,10 +102,17 @@ _f2:
 	ldr r1, address_of_fibout	  	/* r1 ← &fib out */
 	mov r0, #1
 	str r0, [r1]
+	cmp r1, #2						/* Fib <= 2. Branch to menu*/
+	ble _menu 
 	
 	/*Prep Fot the rest of the sequence*/
 	mov r3, #0						/*If Fib in is greater than that, Ready r3 and r4 to prepare the rest of the sequence*/
 	mov r4, #1
+	
+	/*Print Comma*/
+	ldr r0, address_of_comma     	/* r0 ← &Prompt comma */
+	bl printf
+
 	b _menu
 
 _er:
