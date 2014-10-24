cc9c2bd26960cebe4639841e203692154798db0b
maurime2 <maurime2@gmail.com>
2014-10-23 23:56:44 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- midterm/p2-isp.s
+++ midterm/p2-isp.s
@@ -22,7 +22,7 @@ scan_pattern : .asciz "%d"
 
 /* Prompt */
 .balign 8
-prompt: .asciz "An ISP has 3 different subscription packages\n	a)  $30 per month, 11 hours access.  Additional hours\n	    are $3 up to 22 hours then $6 for all additional\n		hours.\n\n	b)  $35 per month, 22 hours access.  Additional hours\n        are $2 up to 44 hours then $4 for each\n		hour above this limit.\n\n	c)  $40 per month, 33 hours access.  Additional hours\n        are $1 up to 66 hours then $2 for each\n\n		hour above this limit.\n\n"
+prompt: .asciz "An ISP has 3 different subscription packages\n	1)  $30 per month, 11 hours access.  Additional hours\n	    are $3 up to 22 hours then $6 for all additional\n		hours.\n\n	2)  $35 per month, 22 hours access.  Additional hours\n        are $2 up to 44 hours then $4 for each\n		hour above this limit.\n\n	3)  $40 per month, 33 hours access.  Additional hours\n        are $1 up to 66 hours then $2 for each\n		hour above this limit.\n\n"
 
 /* Prompt0 */
 .balign 8
