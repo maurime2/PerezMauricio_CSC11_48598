9ef0ce9d64e88fa4476a28ecc3cc7f7b4d712085
maurime2 <maurime2@gmail.com>
2014-10-24 02:10:34 -0700

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- midterm/menu.s
+++ midterm/menu.s
@@ -42,7 +42,7 @@ return: .word 0
 
 /* Divider */
 .balign 8
-divider: .asciz "************** CSC11-48598 MIDTERM MENU **************\n"
+divider: .asciz "************** CSC11-48598 MIDTERM MENU *************\n"
 
 /* Divider 0 */
 .balign 8
