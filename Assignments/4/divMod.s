7081c403839c721584c10e58163e668d20d9d790
maurime2 <maurime2@gmail.com>
2014-12-11 04:22:14 -0800

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- Assignments/4/divMod.s
+++ Assignments/4/divMod.s
@@ -12,11 +12,11 @@ divMod
 
 	/*Temp Store for A and B*/
 	.balign 8
-	TempA: .word 1
+	TempA: .word 0
 	.balign 8
-	TempB: .word 2
+	TempB: .word 0
 	.balign 8
-	TempC: .word 2
+	TempC: .word 0
 
 .text
  
@@ -38,12 +38,37 @@ _divMod:
 	bl printf                       /* call to printf */
 	
 /*DIVISION PROGRAM STARTS HERE*/
+	MOV R4, r1				@ Number to divide / Will hold remainder.
+	MOV R5, r2				@ Number to divide by.
+	MOV R1, #0				@ Number to hold times divided by value.
+	
+_sub:						@ Subtraction Happens HERE.
+	SUBS R4, R4, R5
+	BMI _exit				@ If Negative, branch to exit.
 
+_add:						@ Increment R3 HERE
+	ADD R1, R1, #1			@ Will Hold value of R4/R5
+	BAL _sub				@ Always Branch to _sub
+
+_exit:
+	ADDs R0, R4, R5			@ Corrects the extra subtraction, a%b = R0
+
+	mov r3, r1
+	mov r4, r0
+
+	/*STORE */
+	ldr r0, address_of_TempA	/* TEMP A now represents A/B */
+	ldr r3, [r0]
+	ldr r0, address_of_TempB	/* TEMP B now represents A%B */
+	ldr r4, [r0]
+	
 	/*Load to print*/
 	ldr r3, address_of_TempA	/* TEMP A now represents A/B */
 	ldr r3, [r3]
 	ldr r4, address_of_TempB	/* TEMP B now represents A%B */
-	ldr r4, [r4]
+	ldr r0, [r4]
+	ldr r5, address_of_TempC	/* TEMP B now represents A%B */
+	ldr r5, [r5]
 
 	/*ADD TO COUNT*/
 	add r5, r5, #1
@@ -52,9 +77,10 @@ _divMod:
 
 
 /*Messages*/
-address_of_divPrompt 	: .word divPrompt				/*"Problem Chosen %d"				*/
-address_of_TempA 	: .word TempA				/*"Problem Chosen %d"				*/
-address_of_TempB 	: .word TempB				/*"Problem Chosen %d"				*/
+address_of_divPrompt 	: .word divPrompt			/*"PROMPTS INSIDE DIVMOD"			*/
+address_of_TempA 		: .word TempA				/*"Problem Chosen %d"				*/
+address_of_TempB 		: .word TempB				/*"Problem Chosen %d"				*/
+address_of_TempC 		: .word TempC				/*"Problem Chosen %d"				*/
 
 /*Messages*/
 /* External */
