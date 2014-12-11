012bfceeee33625c1f567000ae0b0eb00d54ec19
maurime2 <maurime2@gmail.com>
2014-12-11 14:22:12 -0800

    Signed-off-by: maurime2 <maurime2@gmail.com>

--- Class/Week9/randTest.s
+++ Class/Week9/randTest.s
@@ -20,7 +20,7 @@ main:
 	mov r1,r0,ASR #1             /* In case random return is negative */
 	mov r2,#90                   /* Move 90 to r2 */
 		                         /* We want rand()%90+10 so cal divMod with rand()%90 */
-	bl divMod                    /* Call divMod function to get remainder */
+@	bl divMod                    /* Call divMod function to get remainder */
 	add r1,#10                   /* Remainder in r1 so add 10 giving between 10 and 99 -> 2 digits */
 	
     ldr r0, address_of_message   /* Set &message2 as the first parameter of printf */
